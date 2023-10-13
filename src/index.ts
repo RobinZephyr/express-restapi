b90ffb10890d481268d77db91f1c604900e34293
Rubean <rubeanrakeru@gmail.com>
2023-10-13 10:59:37 +0800

    2nd

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { Request, Response } from "express"
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
-import userRouter from "./routes/userRouter"
+import express from "express"; // Import express as a function
+import bodyParser from "body-parser";
+import { Request, Response } from "express";
+import { AppDataSource } from "./data-source";
+import userRouter from "./routes/userRouter";
+import cors from 'cors';
 
 AppDataSource.initialize()
     .then(async () => {
+        // MIDDLEWARE
+        const app = express();
 
-    // MIDDLEWARE
-    const app = express();
-    app.use(bodyParser.json());
-    app.use("/public",userRouter);
-    app.listen(3000);
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+        app.use(bodyParser.json());
+        app.use(cors());
 
-}).catch(error => console.log(error))
+        app.use("/public", userRouter);
+        app.listen(3000);
+        console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+    })
+    .catch(error => console.log(error));
